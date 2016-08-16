e03f9feb3acf7c0eed708323734e02674df18b95
someoldguy01 <someoldguy01@a2plcpnl0561.prod.iad2.secureserver.net>
2016-08-16 04:35:43 -0700

    Everything's finally working. INCREDIBLE.

---
+++ add_doctor.php
@@ -0,0 +1,29 @@
+<?php
+// connect to DB
+$file = fopen("./db.txt", "r") or die("Error opening file.");
+$dbinfo = [];
+while (!feof($file)) {
+    $info = trim(fgets($file));
+    array_push($dbinfo, $info);
+}
+$connect = mysqli_connect($dbinfo[0], $dbinfo[1], $dbinfo[2], $dbinfo[3], $dbinfo[4]);
+if (empty($connect)) {
+    die ("mysqli_connect failed " . mysqli_connect_error());
+}
+
+// first get how many doctors there are
+$sql = 'SELECT * from names';
+$result = $connect->query($sql);
+$ids = [];
+if ($result->num_rows > 0) {
+    while ($row = $result->fetch_assoc()) {
+        array_push($ids, $row["ID"]);
+    }
+}
+
+$sql = 'INSERT INTO names VALUES (?, ?, ?, 0)';
+$stmt = mysqli_prepare ($connect, $sql);  // fill in the blank
+mysqli_stmt_bind_param ($stmt, 'iss', count($ids), $_POST["first"], $_POST["last"]);
+mysqli_stmt_execute($stmt);
+mysqli_stmt_close($stmt);
+echo 'SUCCESS';
---
+++ remove_doctor.php
@@ -0,0 +1,42 @@
+<?php
+// connect to DB
+$file = fopen("./db.txt", "r") or die("Error opening file.");
+$dbinfo = [];
+while (!feof($file)) {
+    $info = trim(fgets($file));
+    array_push($dbinfo, $info);
+}
+$connect = mysqli_connect($dbinfo[0], $dbinfo[1], $dbinfo[2], $dbinfo[3], $dbinfo[4]);
+if (empty($connect)) {
+    die ("mysqli_connect failed " . mysqli_connect_error());
+}
+
+$id = $_POST["id"];
+$sql = 'DELETE FROM names WHERE ID=?';
+$stmt = mysqli_prepare ($connect, $sql);  // fill in the blank
+mysqli_stmt_bind_param ($stmt, 'i', $id);
+mysqli_stmt_execute($stmt);
+mysqli_stmt_close($stmt);
+
+
+// get docs
+$sql = 'SELECT * from names';
+$result = $connect->query($sql);
+$fnames = [];
+$lnames = [];
+if ($result->num_rows > 0) {
+    while ($row = $result->fetch_assoc()) {
+        array_push($fnames, $row["FIRST_NAME"]);
+        array_push($lnames, $row["LAST_NAME"]);
+    }
+}
+
+
+for ($i = 0; $i < count($ids); $i++) {
+    // update w/ correct id
+    $sql = 'UPDATE doctor_logger SET ID=' . $i . ' WHERE FIRST_NAME=' . $fnames[$i] . ' AND LAST_NAME=' . $lnames[$i] . ';';
+    $stmt = mysqli_prepare ($connect, $sql);
+    mysqli_stmt_execute($stmt);
+    mysqli_stmt_close($stmt);
+}
+echo 'SUCCESS';
